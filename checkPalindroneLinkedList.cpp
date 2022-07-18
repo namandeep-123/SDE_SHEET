7c412fc77f5f7fed25e1a5dffd6cfc0f02d913b6
Namandeep Singh <namandeepsingh557@gmail.com>
2022-07-18 15:36:05 +0530

    check if Linked List is Palindrone

---
+++ checkPalindroneLinkedList.cpp
@@ -0,0 +1,40 @@
+ListNode *reverseLinkedList(ListNode *head)
+{
+    ListNode *prev = NULL, *nex = NULL, *cur = head;
+
+    while (cur != NULL)
+    {
+        nex = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nex;
+    }
+    return prev;
+}
+
+bool isPalindrome(ListNode *head)
+{
+    if (head == NULL || head->next == NULL)
+        return true;
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    slow->next = reverseLinkedList(slow->next);
+    slow = slow->next;
+    while (slow != NULL)
+    {
+        if (head->val != slow->val)
+            return false;
+        head = head->next;
+        slow = slow->next;
+    }
+
+    return true;
+}
